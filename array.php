1746bd58117335b4b272c625efb8333eaede9129
EdwinPatino <alexanderpatinogaray@gmail.com>
2022-04-10 18:55:23 -0500

    codigoPhp

---
+++ array.php
@@ -0,0 +1,14 @@
+<?php
+    $miarray = array();
+    $miarray[0] = array();
+    $miarray[1] = array();
+    $miarray[2] = array();
+    $miarray[0][0] = "Chau";
+    $miarray[0][1] = "Adios";
+    $miarray[0][2] = "Algo";
+    $miarray[1][1] = "Hello";
+    $miarray[3] = "Bye";
+    $miarray[2][0] = "Hi";
+    print_r($miarray);
+    
+?>
