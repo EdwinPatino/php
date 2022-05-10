460c749f70b4db665371df6a93d95be6d7233a3f
EdwinPatino <alexanderpatinogaray@gmail.com>
2022-05-10 11:59:07 -0500

    cambios

--- formulario_datos_personales/resultado.php
+++ formulario_datos_personales/resultado.php
@@ -1,4 +1,8 @@
 <?php 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
     if($_POST){
         $nombre = $_POST["txtNombre"];
         $apellido = $_POST["txtApellido"];
