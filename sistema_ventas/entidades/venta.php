32b9ed9dd0083135445d07668a97047cfcedd050
EdwinPatino <alexanderpatinogaray@gamil.com>
2022-06-27 15:17:25 -0500

    cambios

--- sistema_ventas/entidades/venta.php
+++ sistema_ventas/entidades/venta.php
@@ -10,6 +10,9 @@ class Venta
     private $preciounitario;
     private $total;
 
+    private $nombre_cliente;
+    private $nombre_producto;
+
     public function __construct()
     {
         $this->cantidad = 0.0;
@@ -137,7 +140,7 @@ class Venta
 
     }
 
-     public function obtenerTodos(){
+    public function obtenerTodos(){
         $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
         $sql = "SELECT 
                     idventa,
@@ -173,6 +176,69 @@ class Venta
             }
         }
         return $aResultado;
+    }
+    public function cargarGrilla(){
+        $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
+       
+        $sql = "SELECT 
+                A.idventa,
+                A.fecha,
+                A.cantidad,
+                A.fk_idcliente,
+                B.nombre as nombre_cliente,
+                A.fk_idproducto,
+                A.total,
+                A.preciounitario,
+                C.nombre as nombre_producto
+            FROM ventas A
+            INNER JOIN clientes B ON A.fk_idcliente = B.idcliente
+            INNER JOIN productos C ON A.fk_idproducto = C.idproducto
+            ORDER BY A.fecha DESC";
+
+        if (!$resultado = $mysqli->query($sql)) {
+            printf("Error en query: %s\n", $mysqli->error . " " . $sql);
+        }
+
+        $aResultado = array();
+        if($resultado){
+            //Convierte el resultado en un array asociativo
+            while($fila = $resultado->fetch_assoc()){
+                $entidadAux = new Venta();
+                $entidadAux->idventa = $fila["idventa"];
+                $entidadAux->fk_idcliente = $fila["fk_idcliente"];
+                $entidadAux->fk_idproducto = $fila["fk_idproducto"];
+                $entidadAux->fecha = $fila["fecha"];
+                $entidadAux->cantidad = $fila["cantidad"];
+                $entidadAux->preciounitario = $fila["preciounitario"];
+                $entidadAux->nombre_cliente = $fila["nombre_cliente"];
+                $entidadAux->nombre_producto = $fila["nombre_producto"];
+                $entidadAux->total = $fila["total"];
+                $aResultado[] = $entidadAux;
+            }
+        }
+        $mysqli->close();
+        return $aResultado;
+    }
+    public function obtenerFacturacionMensual($mes){
+        $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
+        $sql = "SELECT SUM(total) AS total FROM ventas WHERE MONTH(fecha) = $mes";
+        if (!$resultado = $mysqli->query($sql)) {
+            printf("Error en query: %s\n", $mysqli->error . " " . $sql);
+        }
+        $fila = $resultado->fetch_assoc();
+        $mysqli->close();
+        return $fila["total"];
+    }
+
+    public function obtenerFacturacionAnual($anio){
+        $mysqli = new mysqli(Config::BBDD_HOST, Config::BBDD_USUARIO, Config::BBDD_CLAVE, Config::BBDD_NOMBRE, Config::BBDD_PORT);
+        $sql = "SELECT SUM(total) AS total FROM ventas WHERE YEAR(fecha) = $anio";
+        if (!$resultado = $mysqli->query($sql)) {
+            printf("Error en query: %s\n", $mysqli->error . " " . $sql);
+        }
+        $fila = $resultado->fetch_assoc();
+        $mysqli->close();
+        return $fila["total"];
     }
 
 }
--- sistema_ventas/venta-listado.php
+++ sistema_ventas/venta-listado.php
@@ -5,8 +5,12 @@ include_once "entidades/venta.php";
 $pg = "Listado de ventas";
 
 $venta = new Venta();
-$aVentas = $venta->obtenerTodos();
+$aVentas = $venta->cargarGrilla();
 
+/*
+$venta = new Venta();
+$aVentas = $venta->obtenerTodos();
+*/
 include_once("header.php"); 
 ?>
 
@@ -32,8 +36,8 @@ include_once("header.php");
               <tr>
                   <td><?php echo date_format(date_create($venta->fecha), "d/m/Y H:m"); ?></td>
                   <td><?php echo $venta->cantidad; ?></td>
-                  <td><?php echo $venta->fk_idproducto; ?></td>
-                  <td><?php echo $venta->fk_idcliente; ?></td>
+                  <td><a href="producto-formulario.php?id=<?php echo $venta->fk_idproducto; ?>"><?php echo $venta->nombre_producto; ?></a></td>
+                  <td><a href="cliente-formulario.php?id=<?php echo $venta->fk_idcliente; ?>"><?php echo $venta->nombre_cliente; ?></a></td>
                   <td><?php echo $venta->total; ?></td>
                   <td style="width: 110px;">
                       <a href="venta-formulario.php?id=<?php echo $venta->idventa; ?>"><i class="fas fa-search"></i></a>   
