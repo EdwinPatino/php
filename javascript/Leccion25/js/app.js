119a938bcf18ba2835396c6a6505d3421efb2619
EdwinPatino <alexanderpatinogaray@gamil.com>
2022-10-11 22:32:16 -0500

    cambios

--- javascript/Leccion25/js/app.js
+++ javascript/Leccion25/js/app.js
@@ -9,6 +9,8 @@ const egresos = [
 
 let cargarAPP = ()=>{
     cargarCabecero();
+    cargarIngresos();
+    cargarEgresos();
 }
 
 let totalIngresos = ()=>{
@@ -42,4 +44,79 @@ const formatoMoneda =(valor)=>{
 
 const formatoPorcentaje = (valor)=>{
     return valor.toLocaleString('en-US', {style:'percent', minimunFractionDigits:2});
+}
+
+const cargarIngresos =()=>{
+    let ingresosHTML = '';
+    for(let ingreso of ingresos){
+        ingresosHTML += crearIngresoHTML(ingreso);
+    }
+    document.getElementById('lista-ingresos').innerHTML = ingresosHTML;
+}
+
+const crearIngresoHTML =(ingreso)=>{
+    let ingresoHtml = `
+        <div class="elemento limpiarEstilos">
+            <div class="elemento_descripcion">${ingreso.descripcion}</div>
+            <div class="derecha limpiarEstilos">
+                <div class="elemento_valor">+ ${formatoMoneda(ingreso.valor)}</div>
+                <div class="elemento_eliminar">
+                    <button class="elemento_eliminar--btn">
+                        <ion-icon name="close-circle-outline"
+                        onclick="eliminarIngreso(${ingreso.id})"></ion-icon>
+                    </button>
+                </div>
+            </div>
+        </div>
+    `;
+    return ingresoHtml;
+}
+
+//recuperamos el valor de id de onclick de la funcion crearIngresosHTML
+//utlizamos el metodo findIndex para iterar cada objeto que tenga el arreglo
+//luego comparamos el id recuperado con id del arreglo y si coincide
+//el metodo findIndex nos devuelve el indece que encontramos en el arreglo
+const eliminarIngreso =(id)=>{
+    let indiceEliminar = ingresos.findIndex(ingreso => ingreso.id === id);
+    //usamos pasamos el indice encontrado al metodo splice para eliminar el indice en el arreglo principal
+    //el 1 indica la cantidad de elementos a eliminar, este numero se puede usar para indicar la cantidad de elementos 
+    //a eliminar despues del indice encontrado
+    ingresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarIngresos();
+}
+
+const cargarEgresos =()=>{
+    let egresosHTML = '';
+    for(let egreso of egresos){
+        egresosHTML += crearEgresoHTML(egreso);
+    }
+    document.getElementById('lista-egresos').innerHTML = egresosHTML;
+}
+
+const crearEgresoHTML =(egreso)=>{
+    let egresoHtml = `
+        <div class="elemento limpiarEstilos">
+            <div class="elemento_descripcion">${egreso.descripcion}</div>
+            <div class="derecha limpiarEstilos">
+                <div class="elemento_valor">- ${formatoMoneda(egreso.valor)}</div>
+                <div class="elemento_porcentaje">${formatoPorcentaje(egreso.valor/totalEgresos())}</div>
+                <div class="elemento_eliminar">
+                    <button class="elemento_eliminar--btn">
+                        <ion-icon name="close-circle-outline"
+                        onclick="eliminarEgreso(${egreso.id})"></ion-icon>
+                    </button>
+                </div>
+            </div>
+        </div>
+    `;
+    return egresoHtml;
+}
+
+//Igual que se explica en eliminarIngreso
+const eliminarEgreso =(id)=>{
+    let indiceEliminar = egresos.findIndex(egreso => egreso.id === id);
+    egresos.splice(indiceEliminar, 1);
+    cargarCabecero();
+    cargarEgresos();
 }
